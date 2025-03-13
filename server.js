8ae29920028944a1df939fccb8b21bb3d42833d4
Lduarte@744 <duare7444@gmail>
2025-03-13 20:47:29 -0300

    criando minha branch

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import path from 'path'
 
 const app = express()
 const port = 3000
+const _dirname = path.resolve
+
+app.use(express.static(path.join(_dirName, "public")))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send("sdrtfyguhijkolp")
 })
-
+app.get('/views/assinar', (req, res) =>{
+    res.status(200).sendFile(path.join(_dirname, "views", "assinar.html"))
+})
 
 
 app.listen(port, () => {
