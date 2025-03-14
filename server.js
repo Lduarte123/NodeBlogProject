6c226f8847a3318079dfc6bba0be041a2050ed87
mikamoraiss <mikaellymorais13@gmail.com>
2025-03-14 18:34:58 -0300

    adicionando sistema de login

--- server.js
+++ server.js
@@ -1,21 +1,68 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
+import bodyParser from 'body-parser';
 
-const app = express()
-const port = 3000
-const _dirName = path.resolve()
+const app = express();
+const port = 3000;
+const _dirName = path.resolve();
 
-app.use(express.static(path.join(_dirName, "public")))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(_dirName, 'public')));
+
+function carregarUsuarios() {
+    const caminhoDados = path.join(_dirName, 'data', 'usuarios.json');
+    if (fs.existsSync(caminhoDados)) {
+        const dadosArquivo = fs.readFileSync(caminhoDados);
+        return JSON.parse(dadosArquivo);
+    }
+    return [];
+}
+
+function salvarUsuarios(usuarios) {
+    const caminhoDados = path.join(_dirName, 'data', 'usuarios.json');
+    fs.writeFileSync(caminhoDados, JSON.stringify(usuarios, null, 2));
+}
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirName, "views", "home.html"))
-})
+    res.sendFile(path.join(_dirName, 'views', 'home.html'));
+});
 
-app.get("/noticias", (req, res) => {
-    res.status(200).sendFile(path.join(_dirName, "views", "noticias.html"))
-})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(_dirName, 'views', 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const { usuario, senha } = req.body;
+    const usuarios = carregarUsuarios();
+    const usuarioEncontrado = usuarios.find(user => user.usuario === usuario && user.senha === senha);
+
+    if (usuarioEncontrado) {
+        res.send('Login bem-sucedido!');
+    } else {
+        res.send('Usuário ou senha incorretos!');
+    }
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(_dirName, 'views', 'register.html'));
+});
+
+app.post('/register', (req, res) => {
+    const { usuario, senha } = req.body;
+    const usuarios = carregarUsuarios();
+    
+    if (usuarios.find(user => user.usuario === usuario)) {
+        return res.send('Usuário já existe!');
+    }
+
+    const novoUsuario = { usuario, senha };
+    usuarios.push(novoUsuario);
+    salvarUsuarios(usuarios);
+
+    res.send('Usuário registrado com sucesso!');
+});
 
 app.listen(port, () => {
-    console.log("http://localhost:3000")
-})
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
